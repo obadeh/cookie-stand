4f7a3a28a9b06f6ba1b365a3217e556468c75bd8
obada <obadaquraan@yahoo.com>
2019-10-14 09:53:26 +0300

    up

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 
-var seattle ={
+var seattle = {
 
     min: 23,
     max: 65,
-    AvgCookie: 6.3 ,
+    AvgCookie: 6.3,
     cookies: [],
-    randomCust : function() {
+    randomCust: function () {
 
         var range = (this.max) - (this.min);
         var rand = (Math.random() * range) + this.min;
         return Math.ceil(rand);
-        
-        
-     }
-    
+
+
+    }
+
 };
 
 
 
-var Tokyo ={
-    
+var Tokyo = {
+
     min: 3,
     max: 24,
     AvgCookie: 1.2,
     cookies: [],
-    randomCust : function() {
+    randomCust: function () {
         var range = this.max - this.min;
         var rand = (Math.random() * range) + this.min;
         return Math.ceil(rand);
@@ -32,31 +32,31 @@ var Tokyo ={
 }
 
 
-var Dubai	 = {
-    
+var Dubai = {
+
     min: 11,
     max: 38,
-    AvgCookie: 3.7 ,
+    AvgCookie: 3.7,
     cookies: [],
 
-    randomCust : function() {
+    randomCust: function () {
         var range = this.max - this.min;
         var rand = (Math.random() * range) + this.min;
         return Math.ceil(rand);
-        
+
     }
 }
 
 
 
-var Paris ={
-    
+var Paris = {
+
     min: 20,
     max: 38,
-    AvgCookie: 2.3 ,
+    AvgCookie: 2.3,
     cookies: [],
 
-    randomCust : function() {
+    randomCust: function () {
         var range = this.max - this.min;
         var rand = (Math.random() * range) + this.min;
         return Math.ceil(rand);
@@ -66,14 +66,14 @@ var Paris ={
 
 
 
-var Lima ={
-    
+var Lima = {
+
     min: 2,
     max: 16,
-    AvgCookie: 4.6 ,
+    AvgCookie: 4.6,
     cookies: [],
 
-    randomCust : function() {
+    randomCust: function () {
         var range = this.max - this.min;
         var rand = (Math.random() * range) + this.min;
         return Math.ceil(rand);
@@ -86,50 +86,50 @@ var Lima ={
 
 
 var header1 = document.createElement('h1');
-header1.textContent='Sales Data';
+header1.textContent = 'Sales Data';
 
 var asd = document.getElementById("test");
 asd.appendChild(header1);
 
 var paragraph = document.createElement('p');
-paragraph.textContent='seattle';
+paragraph.textContent = 'seattle';
 
 var test = document.getElementById('test');
 test.appendChild(paragraph);
 
-var li=[];
+var li = [];
 var ul = document.createElement('ul');
 test.appendChild(ul);
 
-var shops = [seattle ] ;
-var hours=['6am', '7am', '8am', '9am','10am', '11am','12am'];
-var cookies= [];
-var result=[];
-for(var i=0;i<shops.length;i++){
+var shops = [seattle];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am'];
+var cookies = [];
+var result = [];
+for (var i = 0; i < shops.length; i++) {
 
     var shop = shops[i];
 
-    for(var h=0 ; h<hours.length; h++){ 
+    for (var h = 0; h < hours.length; h++) {
 
 
         var hour = hours[h];
 
-        cookies[h]=Math.ceil((seattle.AvgCookie)*(seattle.randomCust() )) ;
-        shops[i].cookies[h]=cookies[h];
-        
-        result[h]= [hours[h]+':'+cookies[h]+' cookies'] ;
+        cookies[h] = Math.ceil((seattle.AvgCookie) * (seattle.randomCust()));
+        shops[i].cookies[h] = cookies[h];
+
+        result[h] = [hours[h] + ':' + cookies[h] + ' cookies'];
 
 
 
 
         li = document.createElement('li');
 
-         li.textContent=result[h];
+        li.textContent = result[h];
 
 
-         ul.appendChild(li);
+        ul.appendChild(li);
 
-    }   ;
+    };
 }
 
 
